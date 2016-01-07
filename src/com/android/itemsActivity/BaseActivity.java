892924ecc4dca29be30707482b3f0d22ad09f4d3
toughlife <toughlife555@gmail.com>
2016-01-07 17:53:25 +0530

    removed unwanted lines

--- src/com/android/itemsActivity/BaseActivity.java
+++ src/com/android/itemsActivity/BaseActivity.java
@@ -5,18 +5,16 @@ import android.os.Bundle;
 
 import com.android.spideycity.R;
 import com.androidquery.AQuery;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.RequestManager;
 
 public class BaseActivity extends Activity{
 	protected AQuery mAQuery;
-	protected RequestManager mGlide;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mAQuery = new AQuery(getApplicationContext());
-		mGlide = Glide.with(getApplicationContext());
+	
 		overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
 	}
 
