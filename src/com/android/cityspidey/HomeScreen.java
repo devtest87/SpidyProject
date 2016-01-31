18a6d1bb891a2d0d9b03b4a1d56c772776f77a72
toughlife <toughlife555@gmail.com>
2016-02-01 02:11:26 +0530

    not login case added

--- src/com/android/cityspidey/HomeScreen.java
+++ src/com/android/cityspidey/HomeScreen.java
@@ -10,18 +10,20 @@ import org.json.JSONObject;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.tv.TvContract.Channels.Logo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -118,6 +120,7 @@ public class HomeScreen extends BaseActivity implements OnClickListener {
 			
 			@Override
 			public void onClick(View v) {
+				if(PreferenceHelper.getSingleInstance(getApplicationContext()).getBoolean(PreferenceKey.IS_LOGIN))
 				showMenu();
 			}
 		});
@@ -374,9 +377,6 @@ public class HomeScreen extends BaseActivity implements OnClickListener {
 		startActivity(intent);
 	}
 
-	private int imageArra[] = { R.drawable.antartica1, R.drawable.antartica2,
-			R.drawable.antartica3, R.drawable.antartica4,
-			R.drawable.antartica5, R.drawable.antartica6,
-			R.drawable.antartica7, R.drawable.antartica8 };
+	
 
 } 
